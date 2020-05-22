7e5b963681a3b21eafb6f7bf13d27eeb939c98d8
LIMMIHEE <48482259+LIMMIHEE@users.noreply.github.com>
2020-05-22 14:51:13 +0900

    0522 7교시
    템플릿 엔진 Pug 사용

--- Node js/EcpressExample/Test_04.js
+++ Node js/EcpressExample/Test_04.js
@@ -1,8 +1,18 @@
 const express = require('express');
 const app = express();
 
+app.set('views','../views');
+//템플릿 파일이 저장될 디렉토리 지정
+app.set('view engine','pug');
+//사용할 템플릿 엔진 설정
+
+app.locals.pretty = true;
+
 app.use(express.static('public'));
 //정적인 파일이 들어갈 디렉토리를 지정한다.
+
+
+
 
 app.get('/',(req,res)=>{
     res.send(' Hi nodejs');
@@ -33,6 +43,11 @@ app.get('/dynamic',(req,res)=>{
         </body>
     </html>`;
     res.send(output);
+})
+
+app.get('/template',(req,res)=>{
+    res.render('Temp.pug');
+    //템플릿 파일을 보여줄때는 res 아래에 있는 렌더 함수를 사용한다.
 })
 
 app.get('/login',(req,res)=>{

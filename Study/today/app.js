4706e2df761736fa10d1dbd17aef1376b797f9e6
LIMMIHEE <48482259+LIMMIHEE@users.noreply.github.com>
2020-06-18 10:02:52 +0900

    공부용

---
+++ Study/today/app.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+
+
+app.get('/',(req,res)=>{
+    // const out ="";
+    // const num = true || 30;
+    // const n = true && 30;
+    
+
+    // res.send(n);
+})
+
+
+
+app.listen(3000,(req,res)=>{
+    console.log('서버 열림');  
+})

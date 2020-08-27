5118e2e7944fb8009586d36c3f2a31ccf758907e
LIMMIHEE <48482259+LIMMIHEE@users.noreply.github.com>
2020-08-27 10:40:53 +0900

    회원 수정, 삭제, 가입, 메인화면 완성

--- Node js/Web/기타 실습/Join/app.js
+++ Node js/Web/기타 실습/Join/app.js
@@ -2,6 +2,10 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser')
 const mysql = require('mysql');
+var moment = require('moment'); 
+require('moment-timezone'); 
+moment.tz.setDefault("Asia/Seoul"); 
+var date = moment().format('YYYY-MM-DD HH:MM:SS');
 
 app.set("views","./views");
 app.set("view engine","ejs");
@@ -27,13 +31,14 @@ else console.log('DB 연결실패');
 let sql={
     list : 'select * from join_t order by id desc',
     edit : 'select * from join_t where id=?',
-    update : 'update todo set user_id=?,password=?,username=?,email=?,tel=?,regDate=? where id=?'
-
+    update : 'update join_t set user_id=?,password=?,username=?,email=?,tel=?,regDate=? where id=?',
+    delete : 'delete from join_t where id=?',
+    insert : 'insert into join_t (user_id,password,username,email,tel,regDate) values(?,?,?,?,?,?);'
 }
 
 
 app.get('/',(req,res)=>{
-    res.send('Hi Join');
+    res.render('index')
 })
 app.get('/list',(req,res)=>{
     conn.query(sql.list,(err,rows)=>{
@@ -49,14 +54,29 @@ app.get('/edit/:id',(req,res)=>{
     })
 })
 app.post('/edit/:id',(req,res)=>{
-    const _docs = req.params;
-    conn.query(sql.update,[_docs.user_id,_docs.password,_docs.username,_docs.email,_docs.tel,_docs.regDate,_docs.id],(err)=>{
+    const _docs = req.body;
+    conn.query(sql.update,[_docs.user_id,_docs.password,_docs.username,_docs.email,_docs.tel,_docs.regDate,req.params.id],(err)=>{
         if(err){console.log(err); return;}
         res.redirect('/list');
     })
 })
-
-
+app.post('/delete/:id',(req,res)=>{
+    const _id = req.params.id;
+    conn.query(sql.delete,[_id],(err,rows)=>{
+        if(err){console.log(err); return;}
+        res.redirect('/list');
+    })
+})
+app.get('/SignUp',(req,res)=>{
+    res.render('signUp')
+})
+app.post('/SignUp',(req,res)=>{
+    const _docs = req.body;
+    conn.query(sql.insert,[_docs.user_id,_docs.password,_docs.username,_docs.email,_docs.tel,date],(err,rows)=>{
+        if(err){console.log(err); return;}
+        res.redirect('/list');
+    })
+})
 
 app.listen(3000,(req,res)=>{
     console.log('server open');
